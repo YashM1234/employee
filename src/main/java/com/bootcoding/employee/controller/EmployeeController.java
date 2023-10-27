d5b5cdd2e6c629c06c2b8a52eb61c9cfcf1ae68b
YashM1234 <yashmungolerpg@gmail.com>
2023-10-27 23:29:12 +0530

    Employee project, All CRUD Operation, in memory...

--- src/main/java/com/bootcoding/employee/controller/EmployeeController.java
+++ src/main/java/com/bootcoding/employee/controller/EmployeeController.java
@@ -3,7 +3,6 @@ package com.bootcoding.employee.controller;
 import com.bootcoding.employee.model.Employee;
 import com.bootcoding.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,16 +12,19 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    //<------create employee-------->
     @PostMapping("/employee")
     public String createEmployee(@RequestBody Employee employee) {
         return employeeService.createEmployee(employee);
     }
 
+    //<------get all employee data-------->
     @GetMapping("/employee")
     public List<Employee> getAllEmployee() {
         return employeeService.getAllEmployee();
     }
 
+    //<------get employee data by id-------->
     @GetMapping("/employee/empId")
     public Object getEmployee(@PathVariable int empId) {
         try {
@@ -32,16 +34,19 @@ public class EmployeeController {
         }
     }
 
+    //<------get all employee data department wise-------->
     @GetMapping("/employee/department/{department}")
     public List<Employee> getEmployeeByDepartment(@PathVariable String department) {
         return employeeService.getEmployeeByDepartment(department);
     }
 
+    //<------update/modify employee data by id-------->
     @PutMapping("/employee/{empId}")
     public Employee updateEmployee(@PathVariable int empId, @RequestBody Employee employee) {
         return employeeService.updateEmployee(empId, employee);
     }
 
+    //<------Delete employee data by id-------->
     @DeleteMapping("/employee/{empId}")
     public String deleteEmployee(@PathVariable int empId) {
         return employeeService.deleteEmployee(empId);
--- src/main/java/com/bootcoding/employee/repository/EmployeeRepoImpl.java
+++ src/main/java/com/bootcoding/employee/repository/EmployeeRepoImpl.java
@@ -11,6 +11,7 @@ import java.util.Map;
 public class EmployeeRepoImpl implements EmployeeRepo{
     Map<Integer, Employee> employeeMap = new HashMap<>();
 
+    //<------create employee-------->
     @Override
     public String createEmployee(Employee employee) {
         if (existEmployee(employee)) {
@@ -20,6 +21,7 @@ public class EmployeeRepoImpl implements EmployeeRepo{
         return "Employee Inserted Successfully";
     }
 
+    //<------get all employee data-------->
     @Override
     public List<Employee> getAllEmployee() {
         List<Employee> employeesList = new ArrayList<>();
@@ -29,6 +31,7 @@ public class EmployeeRepoImpl implements EmployeeRepo{
         return employeesList;
     }
 
+    //<------get employee data by id-------->
     @Override
     public Object getEmployee(int empId) throws RuntimeException {
         if (existEmployee(empId)) {
@@ -37,6 +40,7 @@ public class EmployeeRepoImpl implements EmployeeRepo{
         throw new RuntimeException("Employee not Exist!");
     }
 
+    //<------get all employee data department wise-------->
     @Override
     public List<Employee> getEmployeeByDepartment(String department) {
         List<Employee> employeeList = new ArrayList<>();
@@ -46,6 +50,7 @@ public class EmployeeRepoImpl implements EmployeeRepo{
         return employeeList;
     }
 
+    //<------update/modify employee data by id-------->
     @Override
     public Employee updateEmployee(int empId, Employee employee) {
         if(existEmployee(empId)){
@@ -55,6 +60,7 @@ public class EmployeeRepoImpl implements EmployeeRepo{
         return null;
     }
 
+    //<------Delete employee data by id-------->
     @Override
     public String deleteEmployee(int empId) {
         if(existEmployee(empId)){
@@ -64,6 +70,7 @@ public class EmployeeRepoImpl implements EmployeeRepo{
         return "Employee Doesn't Exist";
     }
 
+    //<------helper method-------->
     private boolean existEmployee(Employee employee) {
         return existEmployee(employee.getEmpId());
     }
